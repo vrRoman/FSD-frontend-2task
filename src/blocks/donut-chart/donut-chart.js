2fee8dd415d10fe1b1ffb43b76576cdb4d185bbd
Roman Valiev <valromrin@mail.ru>
2020-10-05 16:52:12 +0300

    Added room details page

---
+++ src/blocks/donut-chart/donut-chart.js
@@ -0,0 +1,58 @@
+import 'chart.js/dist/Chart.bundle.min'
+
+let ctx = document.querySelector('.donut-chart__chart canvas').getContext('2d')
+
+let donutChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ['Удовлетворительно', 'Хорошо', 'Великолепно', 'Разочарован'],
+        datasets: [{
+            data: [260, 260, 520, 0],
+            backgroundColor: [
+                (function() {
+                    let gradient = ctx.createLinearGradient(60, 0, 60, 120)
+                    gradient.addColorStop(0, '#BC9CFF')
+                    gradient.addColorStop(1, '#8BA4F9')
+                    return gradient
+                })(),
+                (function() {
+                    let gradient = ctx.createLinearGradient(60, 0, 60, 120)
+                    gradient.addColorStop(0, '#6FCF97')
+                    gradient.addColorStop(1, '#66D2EA')
+                    return gradient
+                })(),
+                (function() {
+                    let gradient = ctx.createLinearGradient(60, 0, 60, 120)
+                    gradient.addColorStop(0, '#FFE39C')
+                    gradient.addColorStop(1, '#FFBA9C')
+                    return gradient
+                })(),
+                'black'
+            ],
+            borderColor: '#fff',
+            borderWidth: 3
+        }]
+    },
+    options: {
+        legend: {
+            display: false
+        },
+        responsive: false,
+        maintainAspectRatio: false,
+        cutoutPercentage: 87
+    }
+})
+
+
+drawValue($('.donut-chart__list li'), document.querySelector('.donut-chart__value'))
+
+function drawValue(listItems, valueItem) {
+    for (let item of listItems) {
+        item.onmouseover = () => {
+            valueItem.innerHTML = `<h1 style="color: ${item.dataset.color}">${item.dataset.value}</h1><h3 style="color: ${item.dataset.color}">голосов</h3>`
+        }
+        item.onmouseout = () => {
+            valueItem.innerHTML = ''
+        }
+    }
+}
