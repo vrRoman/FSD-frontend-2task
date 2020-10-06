b1c174b087ca04e49690b744ebe830f78acd399f
Roman Valiev <valromrin@mail.ru>
2020-10-06 08:22:20 +0300

    Added login and registration page

--- src/pages/website-pages/login-and-registration/login-and-registration.js
+++ src/pages/website-pages/login-and-registration/login-and-registration.js
@@ -1,1 +1,28 @@
- 
+import '@/js/index'
+import '@/sass/main.sass'
+import '@/sass/ui-kit.sass'
+
+import './login-and-registration.pug'
+import './login-and-registration.sass'
+
+import './images/login-registration-bg.jpg'
+
+
+const loginButtons = $('.signup-card__account-exists .button'),
+      signupButtons = $('.signin-card__account-not-exists .button')
+const signinCard = $('.login-and-registration__signin-card'),
+      signupCard = $('.login-and-registration__signup-card')
+
+
+loginButtons.click(showSigninCard)
+signupButtons.click(showSignupCard)
+
+
+function showSigninCard() {
+    signupCard.hide()
+    signinCard.show()
+}
+function showSignupCard() {
+    signupCard.show()
+    signinCard.hide()
+}
