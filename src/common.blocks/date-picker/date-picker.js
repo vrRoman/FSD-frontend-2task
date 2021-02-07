1cee6eb7210d58145897a84f36a8894bc69783d8
Roman Valiev <valromrin@mail.ru>
2021-02-07 20:44:44 +0300

    Changed the date-picker block

--- src/common.blocks/date-picker/date-picker.js
+++ src/common.blocks/date-picker/date-picker.js
@@ -1,133 +1,149 @@
 import 'jquery-ui/ui/widgets/datepicker';
-import '@common.blocks/date-picker/jquery.datepicker.extension.range.min';
+import './jquery.datepicker.extension.range.min';
 
-const clearBtn = '<button class="ui-datepicker-current '
-  + 'ui-state-default ui-corner-all '
-  + 'ui-datepicker-custom-button ui-datepicker-clear-button js-ui-datepicker-clear-button" '
-  + 'type="button">Очистить</button> ';
+class DatePicker {
+  constructor({ element, options = {}, initialDate = [null, null], doubleText = false }) {
+    this.clearBtnHTML = '<button class="ui-datepicker-current '
+      + 'ui-state-default ui-corner-all '
+      + 'ui-datepicker-custom-button ui-datepicker-clear-button js-ui-datepicker-clear-button" '
+      + 'type="button">Очистить</button> ';
+    this.applyBtnHTML = '<button class="ui-datepicker-current '
+      + 'ui-state-default ui-corner-all '
+      + 'ui-datepicker-custom-button ui-datepicker-apply-button js-ui-datepicker-apply-button" '
+      + 'type="button">Применить</button> ';
+    this.monthNames = [
+      'Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
+      'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь',
+    ];
+    this.monthNamesShort = [
+      'янв', 'фев', 'мар', 'апр', 'май', 'июн',
+      'июл', 'авг', 'сен', 'окт', 'ноя', 'дек',
+    ];
+    this.dayNames = [
+      'Воскресенье', 'Понедельник', 'Вторник',
+      'Среда', 'Четверг', 'Пятница', 'Суббота',
+    ];
+    this.dayNamesShort = [
+      'Вс', 'Пн', 'Вт',
+      'Ср', 'Чт', 'Пт', 'Сб',
+    ];
+    this.dayNamesMin = [
+      'Вс', 'Пн', 'Вт',
+      'Ср', 'Чт', 'Пт', 'Сб',
+    ];
 
-const applyBtn = '<button class="ui-datepicker-current '
-  + 'ui-state-default ui-corner-all '
-  + 'ui-datepicker-custom-button ui-datepicker-apply-button js-ui-datepicker-apply-button" '
-  + 'type="button">Применить</button> ';
+    this.$elem = $(element);
+    this.extensionRangeObject = {};
+    this.handleApplyButtonClick = this.handleApplyButtonClick.bind(this);
+    this.handleClearButtonClick = this.handleClearButtonClick.bind(this);
+    this.onSelect = this.onSelect.bind(this);
+    this.doubleText = doubleText;
+    this.defaultOptions = {
+      range: 'period',
+      showButtonPanel: true,
+      currentText: this.clearBtnHTML + this.applyBtnHTML,
+      firstDay: 1,
+      showOtherMonths: true,
+      selectOtherMonths: true,
+      dateFormat: 'dd.mm.yy',
+      monthNames: this.monthNames,
+      monthNamesShort: this.monthNamesShort,
+      dayNames: this.dayNames,
+      dayNamesShort: this.dayNamesShort,
+      dayNamesMin: this.dayNamesMin,
+      onSelect: this.onSelect,
+    };
+    this.options = {
+      ...this.defaultOptions,
+      ...options,
+    };
 
-const monthNames = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
-const monthNamesShort = ['янв', 'фев', 'мар', 'апр', 'май', 'июн', 'июл', 'авг', 'сен', 'окт', 'ноя', 'дек'];
-const dayNames = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
-const dayNamesShort = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'];
-const dayNamesMin = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'];
+    this.init();
+    this.setDate(initialDate);
 
-function checkDateAndUpdateClearBtn($datepicker) {
-  $datepicker.each((i, elem) => {
-    if ($(elem).datepicker('getDate')) {
-      $($(elem).find('.js-ui-datepicker-clear-button')).removeClass('ui-datepicker-clear-button_disabled');
+    this.observers = [];
+  }
+
+  init() {
+    this.$elem.datepicker(this.options);
+  }
+
+  subscribe(observer) {
+    this.observers.push(observer);
+  }
+
+  notify(action) {
+    this.observers.forEach((observer) => {
+      observer.update(action);
+    });
+  }
+
+  getClearBtn() {
+    return $(this.$elem.find('.js-ui-datepicker-clear-button'));
+  }
+
+  getApplyBtn() {
+    return $(this.$elem.find('.js-ui-datepicker-apply-button'));
+  }
+
+  updateClearBtn() {
+    if (this.$elem.datepicker('getDate')) {
+      this.getClearBtn().removeClass('ui-datepicker-clear-button_disabled');
     } else {
-      $($(elem).find('.js-ui-datepicker-clear-button')).addClass('ui-datepicker-clear-button_disabled');
+      this.getClearBtn().addClass('ui-datepicker-clear-button_disabled');
     }
-  });
-}
-
-function applyDate($datepicker, dateTexts, datesObjects) {
-  const $inputs = $datepicker.find('.date-picker__input');
-  if (dateTexts) {
-    if ($inputs.length === 1) {
-      if (dateTexts[0] && dateTexts[1]) {
-        $inputs.val(`${dateTexts[0]} - ${dateTexts[1]}`);
-      }
-    } else if ($inputs.length === 2) {
-      $inputs.each((i, elem) => {
-        $(elem).val(dateTexts[i]);
-      });
-    }
-
-    $datepicker.datepicker('setDate', datesObjects || dateTexts);
-    checkDateAndUpdateClearBtn($datepicker);
   }
 
-  $datepicker.removeClass('date-picker_expanded');
-}
+  setDate(dateObjects, dateTexts = [null, null]) {
+    this.$elem.datepicker('setDate', dateObjects);
+    [this.extensionRangeObject.startDate, this.extensionRangeObject.endDate] = dateObjects;
+    [this.extensionRangeObject.startDateText, this.extensionRangeObject.endDateText] = dateTexts;
 
-function clearDate($datepicker) {
-  const $inputs = $datepicker.find('.date-picker__input');
-  $inputs.val('');
-
-  $datepicker.datepicker('setDate', [null, null]);
-  checkDateAndUpdateClearBtn($datepicker);
-}
-
-function onSelect(dateText, inst, extensionRange) {
-  const $datepicker = inst.dpDiv.parent();
-  let dates = [extensionRange.startDateText, extensionRange.endDateText];
-
-  function handleApplyButtonClick() {
-    applyDate($datepicker, dates, [extensionRange.startDate, extensionRange.endDate]);
-    $($datepicker.find('.js-ui-datepicker-clear-button')).click(() => {
-      clearDate($datepicker);
-      dates = [null, null];
-      $($datepicker.find('.js-ui-datepicker-apply-button')).click(handleApplyButtonClick);
-    });
-    $($datepicker.find('.js-ui-datepicker-apply-button')).click(handleApplyButtonClick);
+    this.getClearBtn().click(this.handleClearButtonClick);
+    this.getApplyBtn().click(this.handleApplyButtonClick);
+    this.updateClearBtn();
   }
 
-  setTimeout(() => {
-    $($datepicker.find('.js-ui-datepicker-apply-button')).click(handleApplyButtonClick);
-    $($datepicker.find('.js-ui-datepicker-clear-button')).click(() => {
-      clearDate($datepicker);
-      dates = [null, null];
-      $($datepicker.find('.js-ui-datepicker-apply-button')).click(handleApplyButtonClick);
+  handleApplyButtonClick() {
+    const dates = [this.extensionRangeObject.startDate, this.extensionRangeObject.endDate];
+    const dateTexts = [
+      this.extensionRangeObject.startDateText, this.extensionRangeObject.endDateText,
+    ];
+
+    this.setDate(dates, dateTexts);
+
+    let valueText;
+    if (this.doubleText) {
+      valueText = dateTexts;
+    } else if (dateTexts[0] && dateTexts[1]) {
+      valueText = `${dateTexts[0]} - ${dateTexts[1]}`;
+    } else {
+      valueText = null;
+    }
+
+    this.notify({
+      type: 'CLICK_APPLY-BUTTON',
+      value: dates,
+      valueText,
     });
-  }, 100);
+  }
+
+  handleClearButtonClick() {
+    this.setDate([null, null]);
+    this.notify({
+      type: 'CLICK_CLEAR-BUTTON',
+      value: [null, null],
+      valueText: null,
+    });
+  }
+
+  onSelect(dateText, inst, extensionRange) {
+    this.extensionRangeObject = extensionRange;
+    setTimeout(() => {
+      this.getApplyBtn().click(this.handleApplyButtonClick);
+      this.getClearBtn().click(this.handleClearButtonClick);
+    }, 100);
+  }
 }
 
-function setDatepickerDate($datepicker, dates) {
-  $datepicker.datepicker('setDate', dates);
-
-  $datepicker.find('.js-ui-datepicker-apply-button').click(() => {
-    applyDate($datepicker, false);
-  });
-  $datepicker.find('.js-ui-datepicker-clear-button').click(() => {
-    clearDate($datepicker);
-    $datepicker.find('.js-ui-datepicker-apply-button').click(() => {
-      applyDate($datepicker, false);
-    });
-  });
-
-  checkDateAndUpdateClearBtn($datepicker);
-}
-
-$('.js-date-picker_single').datepicker({
-  range: 'period',
-  showButtonPanel: true,
-  currentText: clearBtn + applyBtn,
-  firstDay: 1,
-  showOtherMonths: true,
-  selectOtherMonths: true,
-  dateFormat: 'd M',
-  monthNames,
-  monthNamesShort,
-  dayNames,
-  dayNamesShort,
-  dayNamesMin,
-  onSelect,
-});
-
-$('.js-date-picker_double').datepicker({
-  range: 'period',
-  showButtonPanel: true,
-  currentText: clearBtn + applyBtn,
-  firstDay: 1,
-  showOtherMonths: true,
-  selectOtherMonths: true,
-  dateFormat: 'dd.mm.yy',
-  monthNames,
-  monthNamesShort,
-  dayNames,
-  dayNamesShort,
-  dayNamesMin,
-
-  onSelect,
-});
-
-setDatepickerDate($('.js-date-picker'), [null, null]);
-
-export { setDatepickerDate, onSelect };
+export default DatePicker;
