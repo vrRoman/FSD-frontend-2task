207d2a1d37589858db17f9cadb149a9d9edd6392
Roman Valiev <valromrin@mail.ru>
2021-02-09 21:08:32 +0300

    Changed the search-card block

---
+++ src/common.blocks/cards/search-card/search-card.js
@@ -0,0 +1,34 @@
+import DatePicker from '@/common.blocks/date-picker/date-picker';
+import Dropdown from '@/common.blocks/dropdown/dropdown';
+import ItemsCounter from '@common.blocks/items-counter/items-counter';
+
+class SearchCard {
+  constructor(elem) {
+    this.elem = elem;
+
+    this.init();
+  }
+
+  init() {
+    const datePicker = new DatePicker({
+      element: this.getDateDropdown().querySelector('.date-picker'),
+      doubleText: true,
+    });
+    const dateDropdown = new Dropdown(this.getDateDropdown(), datePicker, 'ДД.ММ.ГГГГ');
+
+    const itemsCounter = new ItemsCounter(this.getGuestsDropdown().querySelector('.items-counter'), 'гостя');
+    const guestsDropdown = new Dropdown(this.getGuestsDropdown(), itemsCounter, 'Сколько гостей');
+  }
+
+  getDateDropdown() {
+    const selector = '.search-card__dates .dropdown';
+    return this.elem.querySelector(selector);
+  }
+
+  getGuestsDropdown() {
+    const selector = ' .search-card__guests .dropdown';
+    return this.elem.querySelector(selector);
+  }
+}
+
+export default SearchCard;
