56678bf4989509270d8b4548e5372378a857c131
Roman Valiev <valromrin@mail.ru>
2021-02-09 20:15:42 +0300

    Added RoomCard class

--- src/common.blocks/cards/room-card/room-card.js
+++ src/common.blocks/cards/room-card/room-card.js
@@ -1,9 +1,25 @@
 import 'slick-carousel/slick/slick.min';
 import 'slick-carousel/slick/slick.scss';
 
-$('.js-room-card__slider').slick({
-  dots: true,
-  arrows: true,
-  prevArrow: '<i class="slick-prev slick-arrow material-icons" aria-label="Next"></i>',
-  nextArrow: '<i class="slick-next slick-arrow material-icons" aria-label="Next"></i>',
-});
+class RoomCard {
+  constructor(elem) {
+    this.elem = elem;
+    this.init();
+  }
+
+  getSliderElem() {
+    const sliderSelector = '.room-card__slider';
+    return this.elem.querySelector(sliderSelector);
+  }
+
+  init() {
+    $(this.getSliderElem()).slick({
+      dots: true,
+      arrows: true,
+      prevArrow: '<i class="slick-prev slick-arrow material-icons" aria-label="Next"></i>',
+      nextArrow: '<i class="slick-next slick-arrow material-icons" aria-label="Next"></i>',
+    });
+  }
+}
+
+export default RoomCard;
