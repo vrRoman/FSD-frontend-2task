484a05c00f3ca4691fa93a7b4f0e5826e3ca176b
Roman Valiev <valromrin@mail.ru>
2021-02-14 12:01:17 +0300

    Changed the form-elements page

--- src/pages/ui-kit/form-elements/form-elements.js
+++ src/pages/ui-kit/form-elements/form-elements.js
@@ -3,18 +3,67 @@ import '@/js/index';
 import './form-elements.pug';
 
 import '@/sass/main.sass';
-import '@/sass/ui-kit.sass';
 import './form-elements.sass';
 
-import '@common.blocks/dropdown/dropdown';
-import { setDatepickerDate } from '@common.blocks/date-picker/date-picker';
-import '@common.blocks/text-field/text-field';
-import '@common.blocks/like-button/like-button';
-import '@common.blocks/rate-button/rate-button';
-import '@common.blocks/range-slider/range-slider';
-import '@common.blocks/pagination/pagination';
-import '@common.blocks/list/list';
+import '@/common.blocks/text-field/text-field';
+import Dropdown from '@/common.blocks/dropdown/dropdown';
+import ItemsCounter from '@/common.blocks/items-counter/items-counter';
+import DatePicker from '@/common.blocks/date-picker/date-picker';
+import LikeButton from '@/common.blocks/like-button/like-button';
+import RangeSlider from '@/common.blocks/range-slider/range-slider';
+import Pagination from '@/common.blocks/pagination/pagination';
+import ExpandableCheckboxes from '@/common.blocks/expandable-checkboxes/expandable-checkboxes';
+import Comment from '@/common.blocks/comment/comment';
 
-import './images/avatar_murad.png';
+// Dropdowns
+const $guestsDropdowns = $('.form-elements__guests-dropdown .dropdown');
+$guestsDropdowns.each(function init() {
+  const popupElem = this.querySelector('.items-counter');
+  const itemsCounter = new ItemsCounter(popupElem, 'гостя');
+  const dropdown = new Dropdown(this, itemsCounter, 'Сколько гостей');
+});
 
-setDatepickerDate($('.js-date-picker_single'), [new Date(2019, 7, 19), new Date(2019, 7, 23)]);
+const dateDropdownElem = document.querySelector('.form-elements__date-dropdown .dropdown');
+const datePicker = new DatePicker({
+  element: dateDropdownElem.querySelector('.date-picker'),
+  doubleText: true,
+});
+const dateDropdown = new Dropdown(dateDropdownElem, datePicker, 'ДД.ММ.ГГГГ');
+
+const filterDateDropdownElem = document.querySelector('.form-elements__filter-date-dropdown'
+  + ' .dropdown');
+const filterDatePicker = new DatePicker({
+  element: filterDateDropdownElem.querySelector('.date-picker'),
+  options: {
+    dateFormat: 'd M',
+  },
+});
+const filterDateDropdown = new Dropdown(filterDateDropdownElem, filterDatePicker, 'Дата');
+
+const $facilitiesDropdowns = $('.form-elements__facilities-dropdown .dropdown');
+$facilitiesDropdowns.each(function init() {
+  const popupElem = this.querySelector('.items-counter');
+  const itemsCounter = new ItemsCounter(popupElem);
+  const dropdown = new Dropdown(this, itemsCounter, 'Удобства');
+});
+
+// Like buttons
+const $likeButtonElems = $('.form-elements__like-button .like-button');
+$likeButtonElems.each(function init() {
+  const likeButton = new LikeButton(this);
+});
+
+// Range slider
+const rangeSlider = new RangeSlider(document.querySelector('.range-slider'));
+
+// Pagination
+const pagination = new Pagination(document.querySelector('.pagination'));
+
+// Expandable checkbox list
+const $expandableElems = $('.expandable-checkboxes');
+$expandableElems.each(function init() {
+  const expandable = new ExpandableCheckboxes(this);
+});
+
+// Comment
+const comment = new Comment(document.querySelector('.comment'));
