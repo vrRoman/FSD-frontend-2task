5621f4978a14dec28f424ed969fc368c7de69736
Roman Valiev <valromrin@mail.ru>
2021-02-14 15:50:26 +0300

    Changed the header block

--- src/common.blocks/header/header.js
+++ src/common.blocks/header/header.js
@@ -1,27 +1,92 @@
-const hamburgerSelector = '.js-header__hamburger';
-const menuSelector = '.js-header__menu';
-const displayStyle = 'flex';
-const hamburgerActiveClass = 'header__hamburger_active';
-const showOn = 1200;
+class Header {
+  constructor(elem) {
+    this.elem = elem;
+    this.menu = this.getMenu();
+    this.hamburger = this.getHamburger();
+    this.showHamburgerOn = 1200;
+    this.hiddenMenuClass = 'header__menu_hidden';
+    this.visibleHamburgerClass = 'header__hamburger_visible';
+    this.columnNavClass = 'navigation_in-column';
+    this.updateHeader = this.updateHeader.bind(this);
+    this.handleWindowClick = this.handleWindowClick.bind(this);
 
-$(hamburgerSelector).click(function handlerHamburgerClick() {
-  const menu = $(this).parent().find(menuSelector);
-
-  $(this).toggleClass(hamburgerActiveClass);
-  if (menu.css('display') !== 'none') {
-    menu.css('display', 'none');
-  } else {
-    menu.css('display', displayStyle);
+    this.init();
   }
-});
 
-const handlerWindowResize = () => {
-  if (window.innerWidth >= showOn) {
-    if ($(menuSelector).css('display') === 'none') {
-      $(menuSelector).css('display', displayStyle);
+  getHamburger() {
+    const hamburgerSelector = '.header__hamburger';
+    return this.elem.querySelector(hamburgerSelector);
+  }
+
+  getMenu() {
+    const menuSelector = '.header__menu';
+    return this.elem.querySelector(menuSelector);
+  }
+
+  getNav() {
+    const navSelector = '.navigation';
+    return this.elem.querySelector(navSelector);
+  }
+
+  init() {
+    window.addEventListener('resize', this.updateHeader);
+    window.addEventListener('click', this.handleWindowClick);
+    window.addEventListener('load', this.updateHeader);
+  }
+
+  updateHeader() {
+    if (window.innerWidth >= this.showHamburgerOn) {
+      this.menu.classList.remove(this.hiddenMenuClass);
+      this.hamburger.classList.remove(this.visibleHamburgerClass);
+      this.getNav().classList.remove(this.columnNavClass);
     } else {
-      $(menuSelector).css('display', '');
+      this.menu.classList.add(this.hiddenMenuClass);
+      this.hamburger.classList.add(this.visibleHamburgerClass);
+      this.getNav().classList.add(this.columnNavClass);
     }
   }
-};
-window.addEventListener('resize', handlerWindowResize);
+
+  handleWindowClick(evt) {
+    if (evt.path.includes(this.hamburger)) {
+      this.menu.classList.toggle(this.hiddenMenuClass);
+    } else if (!evt.path.includes(this.menu)) {
+      if (!this.menu.classList.contains(this.hiddenMenuClass)) {
+        evt.stopPropagation();
+        evt.preventDefault();
+        this.menu.classList.add(this.hiddenMenuClass);
+      }
+    }
+  }
+}
+
+export default Header;
+
+
+
+// const hamburgerSelector = '.js-header__hamburger';
+// const menuSelector = '.js-header__menu';
+// const displayStyle = 'flex';
+// const hamburgerActiveClass = 'header__hamburger_active';
+// const showOn = 1200;
+//
+// $(hamburgerSelector).click(function handlerHamburgerClick() {
+//   const menu = $(this).parent().find(menuSelector);
+//
+//   $(this).toggleClass(hamburgerActiveClass);
+//   if (menu.css('display') !== 'none') {
+//     menu.css('display', 'none');
+//   } else {
+//     menu.css('display', displayStyle);
+//   }
+// });
+//
+// const handlerWindowResize = () => {
+//   if (window.innerWidth >= showOn) {
+//     if ($(menuSelector).css('display') === 'none') {
+//       $(menuSelector).css('display', displayStyle);
+//     } else {
+//       $(menuSelector).css('display', '');
+//     }
+//   }
+// };
+// window.addEventListener('resize', handlerWindowResize);
