d2226384a91d3490e5ac601e4f7b0bc43c6cf4a0
Roman Valiev <valromrin@mail.ru>
2021-02-14 11:59:55 +0300

    Changed the range-slider block

--- src/common.blocks/range-slider/range-slider.js
+++ src/common.blocks/range-slider/range-slider.js
@@ -1,29 +1,52 @@
 import 'jquery-ui/ui/widgets/slider';
 
-const rubSymbol = '₽';
+class RangeSlider {
+  constructor(elem) {
+    this.rubSymbol = '₽';
+    this.sliderSelector = '.js-range-slider__slider';
+    this.valueSelector = '.js-range-slider__value';
 
-const sliderSelector = '.js-range-slider__slider';
-const valueSelector = '.js-range-slider__value';
+    this.elem = elem;
+    this.values = [5000, 10000];
+    this.onSlide = this.onSlide.bind(this);
 
-const values = [5000, 10000];
+    this.init();
+  }
 
-const numPrettify = (num) => {
-  const n = num.toString();
-  const separator = ' ';
-  return n.replace(/(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, `$1${separator}`);
-};
+  init() {
+    $(this.getSlider()).slider({
+      range: true,
+      min: 200,
+      max: 15400,
+      values: this.values,
+      slide: this.onSlide,
+    });
+    this.updateValueElem();
+  }
 
-$(sliderSelector).slider({
-  range: true,
-  min: 200,
-  max: 15400,
-  values,
-  slide(event, ui) {
-    $(this).parent()
-      .find(valueSelector)
-      .text(`${numPrettify(ui.values[0]) + rubSymbol} - ${numPrettify(ui.values[1])}${rubSymbol}`);
-  },
-});
+  prettifyNum(num) {
+    const n = num.toString();
+    const separator = ' ';
+    return n.replace(/(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, `$1${separator}`);
+  }
 
-$(sliderSelector).parent().find(valueSelector)
-  .text(`${numPrettify(values[0]) + rubSymbol} - ${numPrettify(values[1])}${rubSymbol}`);
+  getSlider() {
+    return this.elem.querySelector(this.sliderSelector);
+  }
+
+  getValueElem() {
+    return this.elem.querySelector(this.valueSelector);
+  }
+
+  onSlide(event, ui) {
+    this.values = [...ui.values];
+    this.updateValueElem();
+  }
+
+  updateValueElem() {
+    this.getValueElem().innerText = `${this.prettifyNum(this.values[0]) + this.rubSymbol} - `
+      + `${this.prettifyNum(this.values[1])}${this.rubSymbol}`;
+  }
+}
+
+export default RangeSlider;
