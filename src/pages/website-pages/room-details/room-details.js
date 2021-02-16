66f87e3ecf9c955ee9bdc08d02b1709602b4ad03
Roman Valiev <valromrin@mail.ru>
2021-02-16 10:20:15 +0300

    Changed the room-details page

--- src/pages/website-pages/room-details/room-details.js
+++ src/pages/website-pages/room-details/room-details.js
@@ -1,20 +1,28 @@
 import '@/js/index';
 
 import '@/sass/main.sass';
-import '@/sass/ui-kit.sass';
 
 import './room-details.pug';
 import './room-details.sass';
-
-import '@common.blocks/date-picker/date-picker';
-import '@common.blocks/cards/booking-card/booking-card';
-import '@common.blocks/dropdown/dropdown';
-import '@common.blocks/donut-chart/donut-chart';
-import '@common.blocks/like-button/like-button';
-import '@common.blocks/header/header';
 
 import './images/room_1.jpg';
 import './images/room_2.jpg';
 import './images/room_3.jpg';
 import './images/avatar_murad.png';
 import './images/avatar_patricia.png';
+
+import DonutChart from '@/common.blocks/donut-chart/donut-chart';
+import Header from '@/common.blocks/header/header';
+import Comment from '@/common.blocks/comment/comment';
+import BookingCard from '@/common.blocks/booking-card/booking-card';
+
+const chart = new DonutChart(document.querySelector('.donut-chart'));
+
+const header = new Header(document.querySelector('.header'));
+
+const $comments = $('.comment');
+$comments.each(function init() {
+  const comment = new Comment(this);
+});
+
+const bookingCard = new BookingCard(document.querySelector('.booking-card'));
