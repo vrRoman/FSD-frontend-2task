19df1dad7dd4f4fe1d4a7619dd02a83a4e978b4b
Roman Valiev <valromrin@mail.ru>
2021-03-18 10:45:56 +0300

    move login-and-registration logic to class #10

--- src/pages/website-pages/login-and-registration/login-and-registration.js
+++ src/pages/website-pages/login-and-registration/login-and-registration.js
@@ -9,23 +9,64 @@ import './images/login-registration-bg.jpg';
 import '@/common.blocks/signup-card/signup-card';
 import '@/common.blocks/header/init';
 
-const $loginButtons = $('.js-signup-card__account-exists .js-button');
-const $signupButtons = $('.js-signin-card__account-not-exists .js-button');
-const $signinCard = $('.js-login-and-registration__signin-card');
-const $signupCard = $('.js-login-and-registration__signup-card');
-const $pageElem = $('.js-login-and-registration');
-const pageElemLoginClass = 'login-and-registration_login';
+class LoginAndRegistration {
+  constructor() {
+    this.loginButtonsSelector = '.js-signup-card__account-exists .js-button';
+    this.signupButtonsSelector = '.js-signin-card__account-not-exists .js-button';
+    this.signinCardSelector = '.js-login-and-registration__signin-card';
+    this.signupCardSelector = '.js-login-and-registration__signup-card';
+    this.pageSelector = '.js-login-and-registration';
+    this.pageLoginClass = 'login-and-registration_login';
 
-function handleLoginButtonsClick() {
-  $signupCard.hide();
-  $signinCard.show();
-  $pageElem.toggleClass(pageElemLoginClass);
-}
-function handleSignupButtonsClick() {
-  $signupCard.show();
-  $signinCard.hide();
-  $pageElem.toggleClass(pageElemLoginClass);
+    this.$loginButtons = this.getLoginButtons();
+    this.$signupButtons = this.getSignupButtons();
+    this.$signinCard = this.getSigninCard();
+    this.$signupCard = this.getSignupCard();
+    this.$pageElem = this.getPageElem();
+
+    this.handleLoginButtonsClick = this.handleLoginButtonsClick.bind(this);
+    this.handleSignupButtonsClick = this.handleSignupButtonsClick.bind(this);
+
+    this.init();
+  }
+
+  getLoginButtons() {
+    return $(this.loginButtonsSelector);
+  }
+
+  getSignupButtons() {
+    return $(this.signupButtonsSelector);
+  }
+
+  getSigninCard() {
+    return $(this.signinCardSelector);
+  }
+
+  getSignupCard() {
+    return $(this.signupCardSelector);
+  }
+
+  getPageElem() {
+    return $(this.pageSelector);
+  }
+
+  handleLoginButtonsClick() {
+    this.$signupCard.hide();
+    this.$signinCard.show();
+    this.$pageElem.toggleClass(this.pageLoginClass);
+  }
+
+  handleSignupButtonsClick() {
+    this.$signupCard.show();
+    this.$signinCard.hide();
+    this.$pageElem.toggleClass(this.pageLoginClass);
+  }
+
+  init() {
+    this.$loginButtons.click(this.handleLoginButtonsClick);
+    this.$signupButtons.click(this.handleSignupButtonsClick);
+  }
 }
 
-$loginButtons.click(handleLoginButtonsClick);
-$signupButtons.click(handleSignupButtonsClick);
+// eslint-disable-next-line no-unused-vars
+const loginAndRegistration = new LoginAndRegistration();
