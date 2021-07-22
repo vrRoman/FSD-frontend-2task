06fc4a8724daf0a6f2b11d6f6b72787a8f775702
Roman Valiev <valromrin@mail.ru>
2021-07-22 17:34:43 +0300

    remove implicit sass colors imports

--- webpack.config.js
+++ webpack.config.js
@@ -3,39 +3,6 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const createSassRule = ({ isScss = false, isImportedFile = false } = {}) => {
-  const importedFileRegExp = /src\/styles\/_colors\.sass$/;
-  let test = /\.sass$/i;
-  let importVarsString = '@import "@/styles/colors"';
-  if (isScss) {
-    test = /\.scss$/i;
-    importVarsString += ';';
-  }
-
-  let sassLoader = {
-    loader: 'sass-loader',
-    options: {
-      additionalData: importVarsString,
-    },
-  };
-
-  if (isImportedFile) {
-    test = importedFileRegExp;
-    sassLoader = 'sass-loader';
-  }
-
-  return {
-    test,
-    exclude: isImportedFile ? undefined : importedFileRegExp,
-    use: [
-      'style-loader',
-      'css-loader',
-      'resolve-url-loader',
-      sassLoader,
-    ],
-  };
-};
-
 module.exports = {
   entry: {
     bundle: '@/js/index.js',
@@ -67,9 +34,15 @@ module.exports = {
         test: /\.css/,
         use: ['style-loader', 'css-loader'],
       },
-      createSassRule({ isImportedFile: true }),
-      createSassRule(),
-      createSassRule({ isScss: true }),
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'resolve-url-loader',
+          'sass-loader',
+        ],
+      },
       {
         test: /\.pug$/,
         use: [
