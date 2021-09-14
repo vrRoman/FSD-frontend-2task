d889f493586832626feb498463cdd8130fb3bdf7
Roman Valiev <roman.veyl@yandex.ru>
2021-09-14 19:12:45 +0300

    login-and-registration page separated #22

--- src/pages/website-pages/login-and-registration/init.js
+++
@@ -1,6 +0,0 @@
-import LoginAndRegistration from '@/pages/website-pages/login-and-registration/LoginAndRegistration';
-
-$('.js-login-and-registration').each(function init() {
-  const loginAndRegistration = new LoginAndRegistration(this);
-  loginAndRegistration.init();
-});
