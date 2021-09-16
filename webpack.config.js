f0cc1c87ac372ce3ae336952933a3d51dcbea9d8
Roman Valiev <roman.veyl@yandex.ru>
2021-09-16 08:40:55 +0300

    add mini-css-extract-plugin #22

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const { readdirSync, statSync } = require('fs');
 
@@ -28,7 +30,7 @@ const getHtmlWebpackPluginInstances = () => {
   const pugFilePaths = getFilesPath(PAGES_PATH, '.pug');
 
   return pugFilePaths.map((filePath) => new HtmlWebpackPlugin({
-    inject: false,
+    chunks: [path.basename(filePath, path.extname(filePath))],
     hash: true,
     template: filePath,
     filename: `${path.basename(filePath, path.extname(filePath))}.html`,
@@ -44,110 +46,125 @@ const getPagesEntries = () => {
   return entriesObject;
 };
 
-module.exports = {
-  entry: getPagesEntries(),
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
-  },
+module.exports = (env, argv) => {
+  const isDev = argv.mode === 'development';
 
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+  return {
+    entry: getPagesEntries(),
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].js',
     },
-  },
 
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
       },
-      {
-        test: /\.css/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          'style-loader',
-          'css-loader',
-          'resolve-url-loader',
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.pug$/,
-        use: [
-          {
-            loader: 'pug-loader',
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
             options: {
-              hmr: true,
-              reloadAll: true,
-              pretty: true,
+              presets: ['@babel/preset-env'],
             },
           },
-        ],
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        exclude: /favicons/,
-        loader: 'file-loader',
-        options: {
-          name: 'images/[name].[ext]',
         },
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf)$/,
-        loader: 'file-loader',
-        options: {
-          name: 'fonts/[name].[ext]',
+        {
+          test: /\.css/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
         },
-      },
-      {
-        test: /favicons\//,
-        loader: 'file-loader',
-        options: {
-          name: 'favicons/[name].[ext]',
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'resolve-url-loader',
+            'sass-loader',
+          ],
         },
-      },
-      {
-        test: /browserconfig\.xml$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
+        {
+          test: /\.pug$/,
+          use: [
+            {
+              loader: 'pug-loader',
+              options: {
+                hmr: true,
+                reloadAll: true,
+                pretty: true,
+              },
+            },
+          ],
         },
-      },
-      {
-        test: /site\.webmanifest$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          exclude: /favicons/,
+          loader: 'file-loader',
+          options: {
+            name: 'images/[name].[ext]',
+          },
         },
-      },
+        {
+          test: /\.(woff|woff2|eot|ttf)$/,
+          loader: 'file-loader',
+          options: {
+            name: 'fonts/[name].[ext]',
+          },
+        },
+        {
+          test: /favicons\//,
+          loader: 'file-loader',
+          options: {
+            name: 'favicons/[name].[ext]',
+          },
+        },
+        {
+          test: /browserconfig\.xml$/,
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+          },
+        },
+        {
+          test: /site\.webmanifest$/,
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+          },
+        },
+      ],
+    },
+
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: isDev ? '[name].css' : '[name].[contenthash].css',
+        chunkFilename: isDev ? '[id].css' : '[id].[contenthash].css',
+      }),
+
+      new CleanWebpackPlugin(),
+
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+      }),
+
+      ...getHtmlWebpackPluginInstances(),
     ],
-  },
 
-  plugins: [
-    new CleanWebpackPlugin(),
+    optimization: {
+      minimizer: [
+        new CssMinimizerPlugin(),
+      ],
+    },
 
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-    }),
-
-    ...getHtmlWebpackPluginInstances(),
-  ],
-
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    port: 9000,
-    hot: true,
-  },
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      port: 9000,
+      hot: true,
+    },
+  };
 };
